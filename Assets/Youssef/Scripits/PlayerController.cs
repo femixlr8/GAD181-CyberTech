00d848717e80d339605d7b61c2780cd600e4ccd7
Youssef Waeil <youssef.eibrahim@student.saedubai.com>
2024-06-23 21:03:43 +0400

    added player control
    Added  walking left and right
    added jumping

---
+++ Assets/Youssef/Scripits/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D player;
+
+    private float playerSpeed = 10;
+
+    private bool onGround;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+
+        //added Input.getaxis Y to avoid using if statements
+        player.velocity = new Vector2(Input.GetAxis("Horizontal") * playerSpeed, player.velocity.y);
+
+
+     
+
+        //INput for player to jump on space key command
+        if (Input.GetKey(KeyCode.W) && onGround)
+        {
+            Jump();
+        }
+
+    }
+
+    private void Jump()
+    {
+        player.velocity = new Vector2(player.velocity.x, playerSpeed);
+
+        onGround = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+}
