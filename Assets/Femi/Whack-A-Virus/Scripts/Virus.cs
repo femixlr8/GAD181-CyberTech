5000e367f07aa620f41e4744a355a5e7a2c86aa0
femixlr8 <fathima.latheef@student.saedubai.com>
2024-06-29 16:42:59 +0400

    created the basic click to hit functionality

--- Assets/Femi/Whack-A-Virus/Scripts/Virus.cs
+++ Assets/Femi/Whack-A-Virus/Scripts/Virus.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 
 public class Virus : MonoBehaviour
 {
-    //The offset of the spirte to hide it
+    [Header("Graphics")]
+    [SerializeField] private Sprite VirusA;
+    [SerializeField] private Sprite VirusAHit;
+    [SerializeField] private Sprite VirusB;
+    [SerializeField] private Sprite VirusBHit;
+
+    //The offset of the sprite to hide it
     private Vector2 startPosition = new Vector2(0f, -2f);
     private Vector2 endPosition = Vector2.zero;
 
@@ -11,6 +17,10 @@ public class Virus : MonoBehaviour
     private float showDuration = 0.5f;
     private float duration = 1f;
 
+    private SpriteRenderer spriteRenderer;
+
+    //Virus Parameters
+    private bool hittable = true;
     private IEnumerator ShowHide(Vector2 start, Vector2 end)
     {
         //Make sure we start at the start
@@ -49,6 +59,42 @@ public class Virus : MonoBehaviour
     private void Start()
     {
         StartCoroutine(ShowHide(startPosition, endPosition));
+    }
+
+    private void Awake()
+    {
+        //Get references to the components to be used
+        spriteRenderer = GetComponent<SpriteRenderer>();   
+    }
+
+    private void OnMouseDown()
+    {
+        if ( hittable)
+        {
+            spriteRenderer.sprite = VirusAHit;
+            //Stop the animation
+            StopAllCoroutines();
+            StartCoroutine(QuickHide());
+            //Turn off hittable so that player cant keep tapping for score
+            hittable = false;
+        }
+    }
+
+    private IEnumerator QuickHide()
+    {
+        yield return new WaitForSeconds(0.25f);
+
+        //Whist player was waiting it may have spawned again here, so just check that hasnt happened before hiding it, this will stop it flickering in that case
+        if(!hittable)
+        {
+            Hide();
+        }
+    }
+
+    private void Hide()
+    {
+        //Set the appropriate Virus parameters to hide it.
+        transform.localPosition = startPosition;
     }
 }
     
