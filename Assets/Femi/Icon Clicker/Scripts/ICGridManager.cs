fbdb63f270df5232c9926e07e24a0ece80f2d7ca
femixlr8 <fathima.latheef@student.saedubai.com>
2024-07-09 17:49:29 +0400

    created scripts and attached it to respective game objects
    preparing for playtesting

--- Assets/Femi/Icon Clicker/Scripts/ICGridManager.cs
+++ Assets/Femi/Icon Clicker/Scripts/ICGridManager.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ICGridManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject blueIconPrefab;
+    public GameObject redXIconPrefab;
+    public int rows = 5;
+    public int columns = 5;
+
+    private GameObject[,] grid;
+    public int redIconCount { get; private set; }
+
     void Start()
     {
-        
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateGrid()
     {
-        
+        grid = new GameObject[rows, columns];
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < columns; y++)
+            {
+                GameObject icon = Instantiate(blueIconPrefab, new Vector2(x, y), Quaternion.identity);
+                icon.transform.SetParent(transform);
+                grid[x, y] = icon;
+            }
+        }
+
+        PlaceRedIcons();
+    }
+
+    void PlaceRedIcons()
+    {
+        int numberOfRedIcons = Random.Range(1, 5);
+        redIconCount = numberOfRedIcons;
+
+        for (int i = 0; i < numberOfRedIcons; i++)
+        {
+            int x = Random.Range(0, rows);
+            int y = Random.Range(0, columns);
+            if (grid[x, y] != null)
+            {
+                Destroy(grid[x, y]);
+                GameObject redIcon = Instantiate(redXIconPrefab, new Vector2(x, y), Quaternion.identity);
+                redIcon.transform.SetParent(transform);
+                grid[x, y] = redIcon;
+            }
+        }
     }
 }
