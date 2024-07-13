ccb567c0cfe90fcfb2d929b3d115e3c34134ec0a
youssef waeil <ywaeil938@gmail.com>
2024-07-14 01:34:19 +0400

    MIstake- Was on play mode while changing
    was on play mode while  doing some chnages
    added audio to play on command , some problemns are being faced

--- Assets/Youssef/Scripits/DestroyTheFile/BrickSystem.cs
+++ Assets/Youssef/Scripits/DestroyTheFile/BrickSystem.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class BrickSystem : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public AudioClip brickBreaking;
+
+    private AudioSource audioSource;
+
     void Start()
     {
-        
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = brickBreaking;
     }
 
     // Update is called once per frame
@@ -18,6 +25,15 @@ public class BrickSystem : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if(collision.collider.gameObject.tag == ("ball"))
+        {
+            if(brickBreaking != null) 
+            {
+                audioSource.Play();
+            }
+
+            Destroy(gameObject, brickBreaking.length);
+        }
+
     }
 }
