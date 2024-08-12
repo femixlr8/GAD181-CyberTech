0b609534b014a299d555ad33d5b8e0f9d1021630
femixlr8 <fathima.latheef@student.saedubai.com>
2024-08-12 21:33:31 +0400

    troubleshooting and final tweaking

--- Assets/Femi/Password Strength Tester/Scripts/PSTGameManager.cs
+++ Assets/Femi/Password Strength Tester/Scripts/PSTGameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Text;
 
 public class PSTGameManager : MonoBehaviour
 {
@@ -14,7 +15,6 @@ public class PSTGameManager : MonoBehaviour
 
     void Start()
     {
-        Time.timeScale = 1; // Ensure game is running
         UpdateTimer();
         UpdateScore();
         GeneratePasswords();
@@ -25,7 +25,7 @@ public class PSTGameManager : MonoBehaviour
         timer -= Time.deltaTime;
         UpdateTimer();
 
-        if (timer <= 0 && !gameOverPanel.activeSelf && !gameWonPanel.activeSelf) // Check for game over only if the panels are not already active
+        if (timer <= 0)
         {
             GameOver();
         }
@@ -46,7 +46,7 @@ public class PSTGameManager : MonoBehaviour
         score++;
         UpdateScore();
 
-        if (score == buttons.Length)
+        if (score == buttons.Length - 1) // Only win when all correct buttons are pressed
         {
             GameWon();
         }
@@ -59,23 +59,57 @@ public class PSTGameManager : MonoBehaviour
 
     void GameOver()
     {
-        Debug.Log("Game Over");
         gameOverPanel.SetActive(true);
         Time.timeScale = 0; // Pause the game
     }
 
     void GameWon()
     {
-        Debug.Log("Game Won");
         gameWonPanel.SetActive(true);
         Time.timeScale = 0; // Pause the game
     }
 
     void GeneratePasswords()
     {
-        foreach (var button in buttons)
+        int weakPasswordIndex = Random.Range(0, buttons.Length); // Pick a random index for the weak password
+
+        for (int i = 0; i < buttons.Length; i++)
         {
-            button.GeneratePassword();
+            if (i == weakPasswordIndex)
+            {
+                buttons[i].password = GenerateWeakPassword(8);
+            }
+            else
+            {
+                buttons[i].GeneratePassword();
+            }
+            buttons[i].GetComponentInChildren<TMP_Text>().text = buttons[i].password;
         }
+    }
+
+    private string GenerateWeakPassword(int length)
+    {
+        const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string lower = "abcdefghijklmnopqrstuvwxyz";
+        const string digits = "0123456789";
+        const string special = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+        System.Random random = new System.Random();
+        StringBuilder passwordBuilder = new StringBuilder();
+
+        // Randomly decide which character type to omit
+        int omitType = random.Next(0, 4); // 0 = uppercase, 1 = lowercase, 2 = digits, 3 = special
+
+        // Fill the password with characters, omitting one type
+        for (int i = 0; i < length; i++)
+        {
+            if (omitType != 0) passwordBuilder.Append(upper[random.Next(upper.Length)]);
+            if (omitType != 1) passwordBuilder.Append(lower[random.Next(lower.Length)]);
+            if (omitType != 2) passwordBuilder.Append(digits[random.Next(digits.Length)]);
+            if (omitType != 3) passwordBuilder.Append(special[random.Next(special.Length)]);
+        }
+
+        // Truncate to the required length
+        return passwordBuilder.ToString().Substring(0, length);
     }
 }
