840909d795244c508c8a8f9744552e8b70592c4a
LenexTLI <sidneyadb@gmail.com>
2024-08-13 18:06:05 +0400

    Adding UI Assets to the game.

--- Assets/Sidney/Main Game/Scene/Main Menu/MainMenuManager.cs
+++ Assets/Sidney/Main Game/Scene/Main Menu/MainMenuManager.cs
@@ -8,13 +8,11 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     public Button startGameButton;
-    public Button quitGameButton;
 
     void Start()
     {
         // Add listeners to the buttons
         startGameButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(OnStartGameClicked);
-        quitGameButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(OnQuitGameClicked);
     }
 
     // Method to be called when the "Start Game" button is clicked
@@ -22,18 +20,6 @@ public class MainMenuManager : MonoBehaviour
     {
         
         SceneManager.LoadScene("Scene_01");
-    }
-
-    // Method to be called when the "Quit Game" button is clicked
-    void OnQuitGameClicked()
-    {
-        // Quit the application
-        Application.Quit();
-
-        // If running in the Unity Editor, stop play mode
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
     }
 }
     
